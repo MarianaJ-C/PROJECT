5c9363c4e2197183a70994e4148b497bfc00a5cf
MARIANA JAIMES <12345678+TuUsuario@users.noreply.github.com>
2025-05-04 18:30:38 -0600

    Corrige archivo app.py para despliegue en render

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('C:/Users/Acer Vero/OneDrive/Documentos/PROJECT/vehicles_us.csv')  
+df = pd.read_csv('vehicles_us.csv')  
 
 st.title('Análisis de Vehículos Usados')
 
